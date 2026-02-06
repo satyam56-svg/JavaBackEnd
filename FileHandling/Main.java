067697b5a66e4c9371b89d3d45ae90715dd7ef62
satyam56-svg <satyamkumar2351056@gmail.com>
2026-02-07 00:26:36 +0530

    File handling

---
+++ FileHandling/Main.java
@@ -0,0 +1,60 @@
+package FileHandling;
+
+import java.io.IOException;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // try(InputStreamReader isr = new InputStreamReader(System.in)) {
+        //     System.out.println("Enter some letters: ");
+        //     int letter = isr.read();
+        //     while(isr.ready()) {
+        //         System.out.print((char) letter);
+        //         letter = isr.read();
+        //     }
+
+        //     isr.close();
+        //     System.out.println();
+        // } catch(IOException e) {
+        //     System.out.println(e.getMessage());
+        // }
+
+    //     try(FileReader fr = new FileReader("theory.txt")) {
+    //         int letter = fr.read();
+    //         while(fr.ready()) {
+    //             System.out.print((char) letter);
+    //             letter = fr.read();
+    //         }
+    //         System.out.println();
+    //     } catch(IOException e) {
+    //         System.out.println(e.getMessage());
+    //     }
+
+    //    try( BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+    //        System.out.println("You typed: " + br.readLine());
+    //    } catch(IOException e) {
+    //        System.out.println(e.getMessage());
+    //    }
+
+    //    try( BufferedReader br = new BufferedReader(new FileReader("theory.txt"))) {
+    //        while(br.ready()) {
+    //            br.readLine();
+    //        }
+    //        System.out.println("You typed: " + br.readLine());
+    //    } catch(IOException e1) {
+    //        System.out.println(e1.getMessage());
+    //    }
+
+        // OutputStream os = System.out;
+        // try(OutputStreamWriter osw = new OutputStreamWriter(System.out)) {
+        //     osw.write(65);
+        //     osw.write(66);
+        //     osw.write(67); 
+        //     osw.write('\n');
+        // } catch(IOException e) {
+        //     System.out.println(e.getMessage());
+        // }
+
+        
+    }
+}
